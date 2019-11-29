ac29011d8a785de7b4bebb6c905d32c2b62785fe
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2019-11-29 23:51:35 +0200

    update project 04 gradle and migrate to androidX

--- 04 ContactManagerWithRoom/app/src/main/java/com/anushka/androidtutz/contactmanager/MainActivity.java
+++ 04 ContactManagerWithRoom/app/src/main/java/com/anushka/androidtutz/contactmanager/MainActivity.java
@@ -2,15 +2,15 @@ package com.anushka.androidtutz.contactmanager;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,7 +25,6 @@ import com.anushka.androidtutz.contactmanager.db.DatabaseHelper;
 import com.anushka.androidtutz.contactmanager.db.entity.Contact;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
