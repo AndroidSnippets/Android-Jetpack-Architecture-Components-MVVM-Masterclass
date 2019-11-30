4e0c3d721d98a31affa2895afe5c8ecc23797e4c
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2019-11-30 22:53:47 +0200

    complete CRUD functionalities using room in 04

--- 04 ContactManagerWithRoom/app/src/main/java/com/anushka/androidtutz/contactmanager/db/ContactsAppDatabase.java
+++ 04 ContactManagerWithRoom/app/src/main/java/com/anushka/androidtutz/contactmanager/db/ContactsAppDatabase.java
@@ -5,8 +5,8 @@ import androidx.room.RoomDatabase;
 
 import com.anushka.androidtutz.contactmanager.db.entity.Contact;
 
-@Database(entities = {Contact.class},version = 1)
+@Database(entities = {Contact.class},version = 1,exportSchema = false)
 public abstract class ContactsAppDatabase extends RoomDatabase {
 
-    public abstract ContactDAO gatContactDAO();
+    public abstract ContactDAO getContactDAO();
 }
