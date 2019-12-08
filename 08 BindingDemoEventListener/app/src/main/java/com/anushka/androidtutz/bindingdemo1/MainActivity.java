c6c90cf00a93bbc9a03ff72c6fa10166df513494
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2019-12-08 23:04:34 +0200

    use date binding for event listeners in 08

--- 08 BindingDemoEventListener/app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
+++ 08 BindingDemoEventListener/app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
@@ -2,14 +2,19 @@ package com.anushka.androidtutz.bindingdemo1;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.os.ConditionVariable;
+import android.view.View;
+import android.widget.Toast;
 
 import com.anushka.androidtutz.bindingdemo1.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    MainActivityClickHandlers handlers;
 
     private ActivityMainBinding activityMainBinding;
 
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main);
         activityMainBinding.setStudent(getCurrentStudent());
+        handlers = new MainActivityClickHandlers(this);
+        activityMainBinding.setClickHandlers(handlers);
 
     }
 
@@ -30,5 +37,22 @@ public class MainActivity extends AppCompatActivity {
         student.setStudentEmail("alex@gmail.com");
         return student;
 
+    }
+
+
+    public class MainActivityClickHandlers {
+        Context context;
+
+        public MainActivityClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        public void onEnrollButtonClicked (View view){
+            Toast.makeText(context, "Enroll Clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onCancelButtonClicked (View view){
+            Toast.makeText(context, "Cancel Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
